4afe6e3d82083a346aba578dceaf296d2173b743
Knot <70258636+Notties@users.noreply.github.com>
2021-09-27 23:26:17 +0700

    Update Ass5Matries.java

--- src/Ass5Matries.java
+++ src/Ass5Matries.java
@@ -8,7 +8,7 @@ public class Ass5Matries {
 
     public static void main(String[] args) throws Exception {
         a.menu();
-    }
+    }//end main
 
     public void menu() {
         int ch;
@@ -258,4 +258,4 @@ public class Ass5Matries {
         JOptionPane.showMessageDialog(null, out);
 
     }
-}
+}//end class
